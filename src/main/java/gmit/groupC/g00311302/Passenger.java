c2eb913e7ebabef9f8e804db0f4a1681cfcde410
g00311302 <g00311302@gmit.ie>
2020-03-02 15:55:21 +0000

    correct pom.xml

---
+++ src/main/java/gmit/groupC/g00311302/Passenger.java
@@ -0,0 +1,16 @@
+package gmit.groupC.g00311302;
+/*
+    Student class
+    Padraig Ó Cosgora
+    G00311302
+    21/02/2020
+ */
+
+public class Passenger {
+    private String title;
+    private String name;
+    private String ID;
+    private String phoneNumber;
+    private int age;
+
+}
---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,14 @@
+/*
+    StudentTests class
+    Padraig Ó Cosgora
+    G00311302
+    21/02/2020
+ */
+import gmit.groupC.g00311302.Passenger;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+public class PassengerTest {
+
+}
