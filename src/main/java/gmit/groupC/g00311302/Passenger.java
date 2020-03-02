34f3223d05c73f25d7b13eb494fdcef2e53bbc32
g00311302 <g00311302@gmit.ie>
2020-03-02 16:38:14 +0000

    code complete

--- src/main/java/gmit/groupC/g00311302/Passenger.java
+++ src/main/java/gmit/groupC/g00311302/Passenger.java
@@ -9,8 +9,103 @@ package gmit.groupC.g00311302;
 public class Passenger {
     private String title;
     private String name;
-    private String ID;
+    private int ID;
     private String phoneNumber;
     private int age;
 
+    public Passenger() {
+        this.title = "";
+        this.name = "";
+        this.ID = 0000;
+        this.phoneNumber = "";
+        this.age = 0000;
+    }
+
+    public Passenger(String title, String name, int ID, String phoneNumber, int age) {
+        if ((title.toLowerCase().equals("mrs") || title.toLowerCase().equals("mr") || title.toLowerCase().equals("ms"))) {
+            this.title = title;
+        } else {
+            throw new IllegalArgumentException("Invalid title!");
+        }
+        if ((name.length() > 2)) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Invalid name!");
+        }
+        if ((ID >= 1000000000)) {
+            this.ID = ID;
+        } else {
+            throw new IllegalArgumentException("Invalid ID!");
+        }
+        if (phoneNumber.length() > 9) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new IllegalArgumentException("Invalid phoneNumber!");
+        }
+        if (age >= 16) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Invalid age!");
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if ((title.toLowerCase().equals("mrs") || title.toLowerCase().equals("mr") || title.toLowerCase().equals("ms"))) {
+            this.title = title;
+        } else {
+            throw new IllegalArgumentException("Invalid title!");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if ((name.length() > 2)) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Invalid name entry!");
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        if ((ID >= 1000000000)) {
+            this.ID = ID;
+        } else {
+            throw new IllegalArgumentException("Invalid ID number!");
+        }
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() > 9) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new IllegalArgumentException("Invalid phone number length!");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 16) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Passenger is too young too fly!");
+        }
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -11,4 +11,150 @@ import org.junit.jupiter.api.*;
 
 public class PassengerTest {
 
+    private Passenger myPassenger;
+
+    @BeforeAll
+    @DisplayName("Starting tests")
+    public static void print(){
+        System.out.println("Starting tests");
+    }
+
+    @BeforeEach
+    @DisplayName("New instance of Passenger")
+    void init(TestInfo testInfo, TestReporter testReporter)
+    {
+        myPassenger = new Passenger();
+        System.out.println("timestamp = " + testInfo.getDisplayName());
+    }
+
+    @Test
+    @DisplayName("Testing Name Getter and Setter of Passenger")
+    void testGettersAndSetters(TestInfo testInfo)
+    {
+        //myPassenger.setName("John Doe");
+        //assertEquals(myPassenger.getName(),"John Doe");
+        myPassenger.setName("John");
+        assertEquals(myPassenger.getName(),"John");
+    }
+
+    @Test
+    @DisplayName("Invalid Title setter test")
+    void testTitleInvalid(TestInfo testInfo)
+    {
+        myPassenger.setTitle("Mr");
+        assertEquals(myPassenger.getTitle(),"Mr");
+        myPassenger.setTitle("Ms");
+        assertEquals(myPassenger.getTitle(),"Ms");
+        myPassenger.setTitle("Mrs");
+        assertEquals(myPassenger.getTitle(),"Mrs");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            myPassenger.setTitle("Miss");
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Name setter Test")
+    void testNameInvalid(TestInfo testInfo)
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            myPassenger.setName("ze");
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Name setter Test")
+    void testNameSetID(TestInfo testInfo)
+    {
+        myPassenger.setID(1000000000);
+        assertEquals(myPassenger.getID(),1000000000);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            myPassenger.setID(1);
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Name setter Test")
+    void testNameSetAge(TestInfo testInfo)
+    {
+        myPassenger.setAge(17);
+        assertEquals(myPassenger.getAge(),17);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            myPassenger.setAge(1);
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Name setter Test")
+    void testNameSetPhoneNumber(TestInfo testInfo)
+    {
+        myPassenger.setPhoneNumber("0123456789");
+        assertEquals(myPassenger.getPhoneNumber(),"0123456789");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            myPassenger.setPhoneNumber("123123");
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Constructor Test (invalid name)")
+    void testConstructorPassengerName(TestInfo testInfo)
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Mrs", "Ze", 1000000000, "0123456789", 16);
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Constructor Test (invalid title)")
+    void testConstructorPassengerTitle(TestInfo testInfo)
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Miss", "John", 1000000000, "0123456789", 16);
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Constructor Test (invalid ID)")
+    void testConstructorPassengerID(TestInfo testInfo)
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Mrs", "John", 4000, "0123456789", 16);
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Constructor Test (invalid phone)")
+    void testConstructorPassengerPhone(TestInfo testInfo)
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Mrs", "John", 1000000000, "12345", 16);
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid Constructor Test (invalid age)")
+    void testConstructorPassengerAge(TestInfo testInfo)
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Ms", "John", 1000000000, "0123456789", 14);
+        });
+    }
+
+    @Test
+    @DisplayName("Valid Constructor test")
+    void testConstructorValid(TestInfo testInfo)
+    {
+        Passenger passenger1 = new Passenger("Mr", "John Doe", 1000000000, "0123456789",
+                17);
+        assertEquals("John Doe", passenger1.getName());
+        assertEquals("Mr", passenger1.getTitle());
+        assertEquals(1000000000, passenger1.getID());
+        assertEquals("0123456789", passenger1.getPhoneNumber());
+        assertEquals(17, passenger1.getAge());
+    }
+
+    @AfterAll
+    @DisplayName("Testing complete")
+    public static void done(){
+        System.out.println("Testing Complete");
+    }
 }
